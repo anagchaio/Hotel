68045d70365aa745122aa730f94c4c6c481b205e
anagchaio <anagchaio@gmail.com>
2021-05-30 19:57:17 -0300

    first

---
+++ src/com/utn/Main.java
@@ -0,0 +1,8 @@
+package com.utn;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/utn/Menu.java
@@ -0,0 +1,5 @@
+package com.utn;
+
+public class Menu {
+    
+}
