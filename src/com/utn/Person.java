faf9593ba317590f6cd732121cf17deaaa426e00
anagchaio <anagchaio@gmail.com>
2021-05-30 20:38:37 -0300

    Person

---
+++ src/com/utn/Person.java
@@ -0,0 +1,55 @@
+package com.utn;
+
+public class Person {
+    private String name;
+    private String surname;
+    private String dni;
+    private Integer age;
+
+    public Person(String name, String surname, String dni, Integer age) {
+        this.name = name;
+        this.surname = surname;
+        this.dni = dni;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return  "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", dni='" + dni + '\'' +
+                ", age=" + age;
+    }
+}
